0cc4ab9cc80d681119673cae0b2cec5c50b29ac9
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-03 22:41:29 +0100

    Working on

--- server.js
+++ server.js
@@ -3,21 +3,14 @@ var app = express();
 var cookieParser = require('cookie-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var { login, getCurrentSessionAccount } = require('./instagram-scraper');
 require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
 app.use(cookieParser());
 
-var Instagram = require('node-instagram').default;
 var cookie = require('cookie');
-
-const redirectUri = 'http://localhost:' + process.env.PORT + '/auth/instagram/callback';
-
-const instagram = new Instagram({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-});
 
 var status = {
     EMPTY: 0,
@@ -29,11 +22,16 @@ var game = {
     status: status.EMPTY
 };
 
+function getRooms() {
+    return [
+        game
+    ];
+}
+
 io.on('connection', function (socket) {
     var token = cookie.parse(socket.handshake.headers.cookie).acces_token;
     var playerNum = game[token];
     // var playerLoc;
-    // action player dir
     socket.on('move', function (data) {
         if (data.action == 'horizontal') {
             if (playerNum == 1) socket.broadcast.emit('move', { action: 'horizontal', player: playerNum, dir: data.dir });
@@ -41,8 +39,15 @@ io.on('connection', function (socket) {
         } else if (data.action == 'vertical') {
             if (playerNum == 1) socket.broadcast.emit('move', { action: 'vertical', player: playerNum, dir: data.dir });
             else if (playerNum == 2) socket.broadcast.emit('move', { action: 'vertical', player: playerNum, dir: data.dir });
-        }
-        
+        } 
+    });
+
+    
+    socket.on('loggin', function (data) {
+        login(data.username, data.password, function(sessionAccount) {
+            console.log(sessionAccount.account.username);
+            socket.emit('loggin', { sessionId: sessionAccount.sessionId, rooms: getRooms() });
+        })
     });
 
     // socket.on('currPosition', function (location) {
@@ -50,7 +55,11 @@ io.on('connection', function (socket) {
     // });
 });
 
-app.get('/play', function (req, res) {
+app.get('/home', function (req, res) {
+    res.render('index');
+});
+
+app.get('/development', function (req, res) {
     res.render('play', { currPlayer: "player1" });
 });
 
@@ -65,7 +74,6 @@ app.get('/', function (req, res) {
             game[token] = 2;
             game.status = status.FULL;
         }
-        console.log("Token " + game[token]);
         if (game[token] == 1) {
             res.render('play', { currPlayer: "player1" });
         } else if (game[token] == 2) {
@@ -78,25 +86,7 @@ app.get('/', function (req, res) {
     }
 });
 
-// Redirect user to instagram oauth
-app.get('/auth/instagram', (req, res) => {
-    res.redirect(instagram.getAuthorizationUrl(redirectUri, { scope: ['basic'] }));
-});
-
-// Handle auth code and get access_token for user
-app.get('/auth/instagram/callback', async (req, res) => {
-    try {
-        const data = await instagram.authorizeUser(req.query.code, redirectUri);
-        // access_token in data.access_token
-        res.cookie("acces_token", data.access_token, { expire: new Date() + 9999 });
-        res.json(data);
-        console.log(data.access_token);
-    } catch (err) {
-        res.json(err);
-    }
-});
-
-
+// res.cookie("acces_token", data.access_token, { expire: new Date() + 9999 });
 
 http.listen(process.env.PORT , function () {
   console.log('BetYourDignity running on port ' + process.env.PORT);
