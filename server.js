c23abe1b3c3dbed66ab73a09e9459e4a31f53504
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-03 13:36:10 +0100

    Adding .env files testing them

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -6,5 +7,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + process.env.PORT);
 });
