e74b34b82d842ec9c67c35976484c15a96dc5ebd
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-03 14:15:45 +0100

    Separating routes

---
+++ routes.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var Instagram = require('node-instagram').default;
+
+// //Middle ware that is specific to this router
+// router.use(function timeLog(req, res, next) {
+//   console.log('Time: ', Date.now());
+//   next();
+// });
+
+const redirectUri = 'http://localhost:3000/auth/instagram/callback';
+
+const instagram = new Instagram({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+});
+
+router.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+// Redirect user to instagram oauth
+router.get('/auth/instagram', (req, res) => {
+    res.redirect(instagram.getAuthorizationUrl(redirectUri, { scope: ['basic'] }));
+});
+
+// Handle auth code and get access_token for user
+router.get('/auth/instagram/callback', async (req, res) => {
+    try {
+        const data = await instagram.authorizeUser(req.query.code, redirectUri);
+        // access_token in data.access_token
+        res.json(data);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,42 +1,11 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
-const Instagram = require('node-instagram').default;
-
-// Create a new instance.
-const instagram = new Instagram({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-});
-
-const redirectUri = 'http://localhost:3000/auth/instagram/callback';
 
 app.use(express.static(__dirname + '/client'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(require('./routes'));
 
 app.listen(process.env.PORT , function () {
-  console.log('Example app listening on port ' + process.env.PORT);
-});
-
-// Redirect user to instagram oauth
-app.get('/auth/instagram', (req, res) => {
-  res.redirect(instagram.getAuthorizationUrl(redirectUri, { scope: ['basic'] }));
-});
-
-// Handle auth code and get access_token for user
-app.get('/auth/instagram/callback', async (req, res) => {
-  try {
-    const data = await instagram.authorizeUser(req.query.code, redirectUri);
-    // access_token in data.access_token
-    res.json(data);
-  } catch (err) {
-    res.json(err);
-  }
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port ' + process.env.PORT);
+  console.log('BetYourDignity running on port ' + process.env.PORT);
 });
