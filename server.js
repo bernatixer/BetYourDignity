1aa631037ebbefa5ca23dc92a4b894ced7527600
Bernat Torres <bernat.torres.bellido@gmail.com>
2018-11-03 13:48:18 +0100

    Adding public/client files

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/client'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT , function () {
   console.log('Example app listening on port ' + process.env.PORT);
 });
